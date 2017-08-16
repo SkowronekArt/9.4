05cb37c218089790de435cf04efef4ec8dd41682
ANNA Skowronek-Jaskuła <annaskowronek-jaskula@MacBook-Air-ANNA.local>
2017-08-16 21:08:02 +0200

    Rev.1 - corrected number of stars (can't find a way to remove 'undefined' in last line

--- js/script.js
+++ js/script.js
@@ -27,42 +27,42 @@ ma dać wynik:
 
 
 
-1.	Zadeklaruj funkcję drawTree,
-2.	która przyjmuje jeden parametr, 
-3.	ten parametr będzie odzwierciedlał wysokość choinki (ilość wypisanych wierszy w konsoli).
+1.  Zadeklaruj funkcję drawTree,
+2.  która przyjmuje jeden parametr, 
+3.  ten parametr będzie odzwierciedlał wysokość choinki (ilość wypisanych wierszy w konsoli).
 
-4.	Wewnątrz funkcji zadeklaruj pętlę for,
-5.	która wykona się tyle razy, ile mamy poziomów (parametr przekazany do drawTree odpowiada za ilość rysowanych wierszy).
-6.	Żeby zachować porządek, zmienną której będziemy używać do działania pętli nazwij "i"
-7.	Wewnątrz tej pętli, utwórz zmienną star,
-8.	która będzie pustym stringiem (stan początkowy choinki który będzie rozbudowywany w kolejnej pętli).
+4.  Wewnątrz funkcji zadeklaruj pętlę for,
+5.  która wykona się tyle razy, ile mamy poziomów (parametr przekazany do drawTree odpowiada za ilość rysowanych wierszy).
+6.  Żeby zachować porządek, zmienną której będziemy używać do działania pętli nazwij "i"
+7.  Wewnątrz tej pętli, utwórz zmienną star,
+8.  która będzie pustym stringiem (stan początkowy choinki który będzie rozbudowywany w kolejnej pętli).
 
-9.	Następnie, wciąż wewnątrz głównej (zewnętrznej) pętli, zadeklaruj kolejną pętlę for. Tak, pętla w pętli!
-10.	Tym razem, zmienną którą będziemy liczyć ilość iteracji nazwij "j"
-11.	Wewnętrzna pętla ma się ona wykonać zależnie od tego, który wiersz aktualnie drukujemy w konsoli.
-12.	Czyli od tego która to iteracja głównej pętli. Warunek stopu to aktualny poziom (wartość zmiennej "i").
-13.	Wewnątrz drugiej pętli nadpisz zmienną "star" i 
-14.	za każdym razem kiedy pętla będzie wykonana, do aktualnej wartości "star" dodaj kolejną gwiazdkę "star += '*';"
+9.  Następnie, wciąż wewnątrz głównej (zewnętrznej) pętli, zadeklaruj kolejną pętlę for. Tak, pętla w pętli!
+10. Tym razem, zmienną którą będziemy liczyć ilość iteracji nazwij "j"
+11. Wewnętrzna pętla ma się ona wykonać zależnie od tego, który wiersz aktualnie drukujemy w konsoli.
+12. Czyli od tego która to iteracja głównej pętli. Warunek stopu to aktualny poziom (wartość zmiennej "i").
+13. Wewnątrz drugiej pętli nadpisz zmienną "star" i 
+14. za każdym razem kiedy pętla będzie wykonana, do aktualnej wartości "star" dodaj kolejną gwiazdkę "star += '*';"
 
-15.	Po opuszczeniu wewnętrznej pętli wyświetl console.log(star).
+15. Po opuszczeniu wewnętrznej pętli wyświetl console.log(star).
 
-16.	Stwórz nowe repozytorium i wyślij tam swoje zadanie, a link do niego wklej poniżej. Pamiętaj o udostępnieniu repozytorium swojemu mentorowi.
+16. Stwórz nowe repozytorium i wyślij tam swoje zadanie, a link do niego wklej poniżej. Pamiętaj o udostępnieniu repozytorium swojemu mentorowi.
 
 
 Dla ambitnych: na każdym z poziomów dodaj spacje tak, aby choinka stała prosto.
 
 */
-var treeHigh = prompt("ile?", "9");
+var treeHigh = prompt("ile?", "3");
 
 function drawTree(treeHigh) {
-	for (var i = 1; i < treeHigh; i++) {
-		var star = " ";
-		for (var j = 0; j < i; j++) {
-			var stars = star += "*";
-		}
-		console.log(stars);
-	}
-	
+    for (var i = 1; i <= treeHigh; i++) {
+        var star = "";
+        for (var j = 0; j < i; j++) {
+            var stars = star += "*";
+        }
+        console.log(stars);
+    }
+//      console.log(stars);
 }
 console.log(drawTree(treeHigh));
 
@@ -74,14 +74,14 @@ var i,
 function drawTree(i) {
     for (var i = 1; i < treeHigh; i++) {
  
-	 	var star = " ";
+        var star = " ";
 
- 		for (var j = 0; j < i; j++) {
- 			var stars = star += '* ';
- 		}
+        for (var j = 0; j < i; j++) {
+            var stars = star += '* ';
+        }
 
- 		console.log(stars);
-	}
+        console.log(stars);
+    }
 
 }
 
