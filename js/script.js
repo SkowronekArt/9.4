b5784a97e4ae20dbfb6f769043c6131f5299c05e
ANNA Skowronek-Jaskuła <annaskowronek-jaskula@MacBook-Air-ANNA.local>
2017-08-17 17:04:28 +0200

    Rev 2. Na pewno nie o to chodziło, ale co ja poradzę, że działa! :)

--- js/script.js
+++ js/script.js
@@ -1,32 +1,4 @@
 /*
-
-Napisz funkcję, która będzie wyświetlać choinkę dowolnej wysokości w konsoli.
-
-Polecenie:
-drawTree(1)
-ma dać wynik:
-
-*
-
-Polecenie:
-drawTree(2)
-ma dać wynik:
-
-*
-**
-
-Polecenie:
-drawTree(5)
-ma dać wynik:
-
-*
-**
-***
-****
-*****
-
-
-
 1.  Zadeklaruj funkcję drawTree,
 2.  która przyjmuje jeden parametr, 
 3.  ten parametr będzie odzwierciedlał wysokość choinki (ilość wypisanych wierszy w konsoli).
@@ -45,13 +17,26 @@ ma dać wynik:
 14. za każdym razem kiedy pętla będzie wykonana, do aktualnej wartości "star" dodaj kolejną gwiazdkę "star += '*';"
 
 15. Po opuszczeniu wewnętrznej pętli wyświetl console.log(star).
-
-16. Stwórz nowe repozytorium i wyślij tam swoje zadanie, a link do niego wklej poniżej. Pamiętaj o udostępnieniu repozytorium swojemu mentorowi.
-
-
+16. Stwórz nowe repozytorium i wyślij tam swoje zadanie, a link do niego wklej poniżej. 
 Dla ambitnych: na każdym z poziomów dodaj spacje tak, aby choinka stała prosto.
 
 */
+
+
+
+/*          rev 1:
+
+1) nazwy zmiennych z małych liter.
+
+2) parametr funkcji powinien zostać użyty jako wysokość choinki.
+
+3) usuń niepotrzebne spacje wewnątrz okrągłych nawiasów fora. Tu możesz zobaczyć jak taki for jest "ospacjowany":
+https://www.w3schools.com/js/js_conventions.asp
+
+4) ostatni console.log nie jest potrzebny. Poza tym, wywołujesz w nim funkcję z parametrem i, który w tym kontekście jest undefined.
+
+
+            rev 1 result:
 var treeHigh = prompt("ile?", "3");
 
 function drawTree(treeHigh) {
@@ -66,24 +51,47 @@ function drawTree(treeHigh) {
 }
 console.log(drawTree(treeHigh));
 
-/*
-var i,
-    j, 
-    treeHigh = prompt("ile", "9");
+*/
 
-function drawTree(i) {
-    for (var i = 1; i < treeHigh; i++) {
- 
-        var star = " ";
 
+/*          rev 2:
+
+@4) nasza funkcja nic nie zwraca, stąd wywołanie console.log(drawTree(treeHigh)) wypisuje na konsolę undefined.
+Zauważ, że nie musimy wypisywać "wyniku" tej funkcji, gdyż już w niej wypisujemy naszą choinkę. Stąd ostatni log jest zbędny.
+
+5) Możesz przenieść var treeHigh = prompt("ile?", "3") na koniec, przed wywołanie funkcji.
+Miej na uwadze jedną prawidłowość: nazwa zmiennej jest przypadkowo zbieżna z nazwą parametru.
+
+
+            rev 2 result:
+*/
+
+var treeHigh = prompt("ile", "3")
+
+function drawTree(n) {
+    for (var i = 1; i <= n; i++) {
+        var star = "";
         for (var j = 0; j < i; j++) {
-            var stars = star += '* ';
+            var star = star += "*";
         }
-
-        console.log(stars);
+        console.log(star);
     }
 
+    return "*";
 }
+drawTree(treeHigh);
 
-console.log(drawTree(i));
+
+//                  WORKS LIKE CHARM!!!!!!!!!               
+
+
+/*
+var n = prompt("ile", "3")
+for (var i = 1; i <= n; i++) {
+    var star = "";
+    for (var j = 0; j < i; j++) {
+        var star = star += "*";
+    }
+    console.log(star);
+}
 */
